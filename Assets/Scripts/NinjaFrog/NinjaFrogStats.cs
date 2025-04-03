c3639ff247c014b76eb2070bcaaecd2d65210eb6
Yogur <alej4ndro5mol@gmail.com>
2025-04-03 11:42:05 +0200

    EditMode metodos rellenos?

--- Assets/Scripts/NinjaFrog/NinjaFrogStats.cs
+++ Assets/Scripts/NinjaFrog/NinjaFrogStats.cs
@@ -1,3 +1,4 @@
+using System.Security.Cryptography;
 using UnityEngine;
 
 [System.Serializable]
@@ -6,9 +7,15 @@ public class NinjaFrogStats
     public int maxLives = 3;          
     public float speed = 5f;        
     public float jumpForce = 10f;    
-    public int attackPower = 1;     
+    public int attackPower = 1;
+    public int currentLives = 3;
 
-    public int attackRange = 1;       
+    public int attackRange = 1;
+
+    public int energia = 50;
+
+    //Valor entre 0 y 1
+    public float reduccionDeDano = 0.5f;
 
     public bool CanAttack(int attackPower, int attackRange)
     {
@@ -18,4 +25,35 @@ public class NinjaFrogStats
         }
         return false;
     }
+
+    //Devuelve la cantidad de vida que perdemos al dar un puñetazo con intensidad “fuerza”
+    public int darPuñetazo(int fuerza)
+    {
+        return (int)(fuerza*reduccionDeDano);
+    }
+
+    //Devuelve true si en la pelea con ”p2”; resulto ganador. Falso en otro caso. Ganará quien tenga más fuerza
+    bool peleaCon(NinjaFrogStats p2)
+    {
+        return p2.maxLives/(attackPower*p2.reduccionDeDano) > maxLives/(p2.attackPower*reduccionDeDano);
+    }
+
+    //Devuelve la fuerza de un personaje. No puede haber fuerzas negativas.
+    float getFuerza()
+    {
+        return attackPower;
+    }
+    
+    //Devuelve true si estas vivo. False en otro caso. El personaje estará vivo si tiene fuerza (fuerza  > 0) 
+    bool estaVivo()
+    {
+        return currentLives > 0;
+    }
+    
+    // Devuelve true si tiene al menos 50 puntos de energía.
+    bool PuedeHacerAtaqueEspecial()
+    {
+        return energia >= 50;
+    }
+
 }
